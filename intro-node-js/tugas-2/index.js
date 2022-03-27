3fc08ad1670bc27647f90f36687bd64eb7ad91bf
StvJo <sjon10422@gmail.com>
2022-03-27 16:49:32 +0700

    add intro-node-js TA files

---
+++ intro-node-js/tugas-2/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+  const query = url.parse(req.url, true);
+  const filename = "." + query.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("Hello World");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8000);
